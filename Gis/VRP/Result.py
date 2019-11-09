db2eae348407fc75e73c454dea46837502163d62
Xingwei <1179853281@qq.com>
2019-11-09 12:48:53 +0800

    give some helpful&relative class

---
+++ Gis/VRP/Result.py
@@ -0,0 +1,36 @@
+"""
+define the result
+"""
+
+class Result:
+    """
+    define the output result class
+    """
+
+    def __init__(self, is_solved, objective_value, x_value, status):
+        self.is_solved = is_solved
+        self.obj_value = objective_value
+        self.x_value = x_value
+        self.status = status
+
+    def __getitem__(self, item):
+        if item in self.__dict__:
+            return self.__dict__[item]
+
+    # instance c['name']/c['coefficients']/c['right'] can get the corresponding value
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def print_result(self):
+        print(self.is_solved)
+        print(self.obj_value)
+        print(self.x_value)
+
+
+if __name__ == '__main__':
+    BOOLEAN = True
+    OBJ = 20
+    X = [2, 3, 4]
+    ST = '101'
+    RES = Result(BOOLEAN, OBJ, X, ST)
+    print('Result:\n', RES['isSolved'], RES['objValue'], RES['xValue'])
---
+++ Gis/VRP/Variable.py
@@ -0,0 +1,30 @@
+"""
+define the variable feature of the problem 
+"""
+class Variable:
+    """
+    define the variable
+    """
+    def __init__(self, type_of_num, bound, name):
+        self.type = type_of_num
+        self.lower_bound = bound[0]
+        self.upper_bound = bound[1]
+        self.name = name
+    
+    def __getitem__(self, item):
+        if item in self.__dict__:
+            return self.__dict__[item]
+
+    # instance a['name']/a['LowerBound']/a['UpperBound'] can get the corresponding value
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+    
+    def __setattr__(self, name, value):
+        return super().__setattr__(name, value)
+
+if __name__ == '__main__':
+    VARIABLE_LIST = []
+    for i in range(3):
+        # 'y%d' % i 产出名称序列
+        VARIABLE_LIST.append(Variable('ICI'[i], [i, float('inf')], 'x%d' % (i+1)))
+    print(VARIABLE_LIST[0].name,VARIABLE_LIST[0].lower_bound,VARIABLE_LIST[0].upper_bound)
