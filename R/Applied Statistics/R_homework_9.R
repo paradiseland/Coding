52f66cf2a20327b48088061b32bd7afc4b38295f
Xingwei <1179853281@qq.com>
2019-11-14 09:19:19 +0800

    polish the code

--- R/Applied Statistics/R_homework_9.R
+++ R/Applied Statistics/R_homework_9.R
@@ -5,12 +5,9 @@ colnames(data_9) <- c('x1','x2','x3','x4','G')
 
 X<- data.frame(x1=78.3563,x2=0.8895,x3=1.8001,x4=14.1022)
 
-data_9_1 <- subset(data_9,G==1)
-data_9_2 <- subset(data_9,G==2)
 
-
-library(MASS)
 # linear discriminant
+library(MASS)
 (lda_co <- lda(G ~ x1+x2+x3+x4,data=data_9))
 G_ <- predict(lda_co)
 newG_lda <- G_$class
@@ -23,17 +20,17 @@ sum(diag(prop.table(tab_lda)))
 G_qda <- predict(qda_co)
 newG_qda <- G_qda$class
 cbind(data_9$G,G_$x,newG_qda)
-tab_qda <- table(data_9$G,newG_qda)
+(tab_qda <- table(data_9$G,newG_qda))
 sum(diag(prop.table(tab_qda)))
 
 # distance discriminant
 library(mvstats)
 dist_co <- discrim.dist(cbind(data_9$x1,data_9$x2,data_9$x3,data_9$x4),as.factor(data_9$G))
-tab_dist <- table(data_9$G,dist_co$newG)
+(tab_dist <- table(data_9$G,dist_co$newG))
 sum(diag(prop.table(tab_dist)))
 
 # bayes discriminant
-bayes_co <- lda(G ~ x1+x2+x3+x4,data=data_9,prior=c(8,7)/15)
+(bayes_co <- lda(G ~ x1+x2+x3+x4,data=data_9,prior=c(8,7)/15))
 
 # make a discriminant on a concrete data
 (G_pre_lda <- predict(lda_co,X))
@@ -42,7 +39,4 @@ bayes_co <- lda(G ~ x1+x2+x3+x4,data=data_9,prior=c(8,7)/15)
 
 (G_pre_dist <- discrim.dist(cbind(data_9$x1,data_9$x2,data_9$x3,data_9$x4),as.factor(data_9$G),X))
 
-(G_pre_dist <- predict(bayes_co,X))
-
-
-G_pre_bayes <- predict()
+(G_pre_bayes <- predict(bayes_co,X))
