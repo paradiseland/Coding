a25a2a75849c27b7b9abb0eedaed255ea56f91fa
Xingwei <1179853281@qq.com>
2020-04-16 16:49:36 +0800

    alg 1

--- Algorithm/chapter1/insert_sort.py
+++ Algorithm/chapter1/insert_sort.py
@@ -3,16 +3,17 @@ def insert_sort(a):
 	insertion sort algorithm.
 	"""
 	for j in range(1, len(a)):
+		# insert the key numbei into the list
 		key = a[j]
 		i = j - 1
-		while i > 0 and a[i] > key:
+		while i >= 0 and a[i] > key:
 			a[i+1] = a[i]
 			i = i-1
 		a[i+1] = key
-	return a
+	return a         
 
 
 if __name__ == "__main__":
-	l = [2,7,4,6,3] 
+	l = [6,5,4,3,2,1] 
 	print(insert_sort(l))
 	
